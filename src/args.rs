106b90e6275983bf180d77d999f73ee486d009ce
Guanran Wang <guanran928@outlook.com>
2025-04-18 00:25:53 +0800

    feat: add interactive mode

--- src/args.rs
+++ src/args.rs
@@ -1,9 +1,7 @@
 use clap::Parser;
 
-use clap_stdin::MaybeStdin;
-
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    pub word: MaybeStdin<String>,
+    pub word: Option<String>,
 }
