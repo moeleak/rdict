f2e2c9e12eccf8bd720ad8e7a7eb60b7be50b83a
Guanran Wang <guanran928@outlook.com>
2025-06-19 17:28:34 +0800

    chore(cli): rename INPUT_TEXT to TEXT in --help

--- rdict-cli/src/args.rs
+++ rdict-cli/src/args.rs
@@ -3,6 +3,7 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 pub struct Args {
+    #[arg(value_name = "TEXT")]
     pub input_text: Option<String>,
 
     /// Cache translations
